cdf2909ec5243d5ac3deff495a9d93b9ac199cae
Pendragon <knownaspendragon@gmail.com>
2025-07-07 16:38:40 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./**/*.{html,js}",
+    "./node_modules/flowbite/**/*.js"
+  ],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: '16px'
+      },
+      colors: {
+        primary: '#0B2447',
+        secondary: '#DC3535',
+        linked: '#0A66C2',
+        insta: '#E4405F',
+        linux: '#FCC624',
+        docker: '#2496ED',
+        nest: '#E0234E',
+        tailwind: '#06B6D4',
+        kube: '#326CE5',
+        third: '#C2DEDC'
+      },
+      fontFamily: {
+        firaSans: "'Fira Sans', sans-serif",
+        firaCode: "'Fira Code', monospace"
+      },
+      screens: {
+        '2xl': '1320px'
+      }
+    },
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
